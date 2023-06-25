3f0d837e955116bfdc72953d5ae909a99b441a28
Dan Stronge <dnlstronge@gmail.com>
2023-06-25 18:30:53 +0100

    [x] - data fetched from db now prints to screen

--- src/pages/[componentId].js
+++ src/pages/[componentId].js
@@ -5,18 +5,33 @@ export default function Page() {
   const router = useRouter()
 
   const [id, setId] = useState('')
+  const [stringData, setStringData] = useState()
 
   console.log(id);
+  let component = '' // Query the DB with the ID to get this
 
   useEffect(() => {
+    const getData = async(uid) => {
+      const response = await fetch(`https://testdb-fc7b9-default-rtdb.europe-west1.firebasedatabase.app/component/${uid}/.json`)
+      const data = await response.json()
+      setStringData(data)
+    }
+
+
     if(router.isReady){
       setId(router.query.componentId)
+      getData(router.query.componentId)
+
     }
   }, [router])
 
+
+
+
   return (
     <main className=''>
-      {id}
+    {stringData}
+      
     </main>
   )
 }
