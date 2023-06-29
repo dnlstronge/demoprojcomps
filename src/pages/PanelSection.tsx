6ed7e419beb4601256a4024f4bebd8f5c51cd1bc
Dan Stronge <dnlstronge@gmail.com>
2023-06-29 07:49:32 +0100

    [chore] - comment

--- src/pages/PanelSection.tsx
+++ src/pages/PanelSection.tsx
@@ -5,6 +5,8 @@ import "../app/globals.css"
 
 
 /* so app =====> posts on live server =====> rendered In Iframe here */
+// front end inits node which runs proj + sends server data to front-end. 
+// preview displays preview in template, gets specific route and renders
 
 const PanelSection = () => {
 
@@ -18,6 +20,10 @@ const PanelSection = () => {
     
   return (
     <div className={classes.container}>
+       <section className={classes.inputSection}>
+        <textarea className={classes.input}></textarea>
+        <button onClick={setIframe} className={classes.prevBtn}>Test Route</button>
+      </section>
       <section className={classes.inputSection}>
         <textarea className={classes.input}></textarea>
         <button onClick={setIframe} className={classes.prevBtn}>Preview File</button>
@@ -25,7 +31,7 @@ const PanelSection = () => {
 
       <section>
        {/* I FRAME GOES HERE */}
-       <iframe title="Testing Iframe" src={stringInput} width="540" height="450"></iframe>
+       <iframe className={classes.iframe} title="Testing Iframe" src={stringInput}></iframe>
       </section>
     
     </div>
